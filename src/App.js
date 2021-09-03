47aaa1f61fcda50925594faeaedf5f57b4c7048a
cse-mkamble <cse.mkamble@gmail.com>
2021-09-03 12:34:23 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import useGeoLocation from "./useGeoLocation";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
 
-  const location = useGeoLocation();
+  const [latt, setLatt] = useState("")
+  const [lngt, setLngt] = useState("")
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+      setLatt(position.coords.latitude)
+      setLngt(position.coords.longitude)
+    });
+  }, [])
 
   return (
     <div>
-      <h2>Your Loaction</h2>
-      <br/>
-      {
-        location.loaded ? JSON.stringify(location) : "Location data not available yet."
-      }
+      <div>
+        <h4>get Lattitude and Longitude</h4>
+        <h4>(Lattitude,Longitude) : {latt},{lngt}</h4>
+      </div>
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
